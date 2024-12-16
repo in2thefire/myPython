57eaf66cc9579964b459adc3d74f0d5b8535b506
Bohdan Kyrylko <in2thefire.code@gmail.com>
2024-12-16 16:29:00 +0100

    while

--- 2day_while.py
+++ 2day_while.py
@@ -1,5 +1,24 @@
+#1 Друк чисел від 1 до 5
 count = 1
 
 while count <= 10:
     print(count)
     count += 1 # Збільшуємо змінну на 1
+
+#2 Нескінченний цикл (завершення через break)
+
+while True:  # Нескінченний цикл
+    user_input = input("Введіть 'стоп' для завершення: ")
+    if user_input.lower() == "стоп":
+        print("Цикл завершено")
+        break  # Завершує цикл
+
+#3 Пропуск ітерації (continue)
+
+number = 0
+
+while number < 10:
+    number += 1
+    if number % 2 == 0:
+        continue  # Пропускає парні числа
+    print(number)  # Виведе 1, 3, 5, 7, 9
